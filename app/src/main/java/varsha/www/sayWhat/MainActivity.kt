8507564bd49ffd6c77b7be73b7089b9c213b5049
varsha gautham <varshagautham79@gmail.com>
2019-10-05 21:49:18 -0700

    firsr

---
+++ app/src/main/java/varsha/www/sayWhat/MainActivity.kt
@@ -0,0 +1,12 @@
+package varsha.www.sayWhat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
